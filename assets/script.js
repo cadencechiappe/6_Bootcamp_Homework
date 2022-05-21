aabe164e981ec12ede8c4ca329f90c73d1ce9727
Cadence Chiappe <cadence@chiappe.com>
2022-05-21 10:53:24 -0500

    fixed same day weather fetch, styled dynamic HTML, added moment.js library script tag

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,7 @@
 var dayContainer = document.getElementById('cityContainer')
 var forecastContainer = document.getElementById('forecastCard')
 var submitBtn = document.getElementById("submitBtn");
-var locationInput = document.getElementById('cityInput').value
+let locationInput = document.getElementById('cityInput')
 
 localStorage.getItem ('cityInput')
 
@@ -19,7 +19,8 @@ function fetchWeatherData () { //retrieves user input and attaches it to url as 
   .then(res => res.json())
   .then(function (data){
     var forecast = data.main
-    // var icon = data.weather[0].icon
+    var icon = data.weather[0].icon
+    var wind = data.wind
 
     var locationInputTitle = document.createElement('h2')
     var weatherDate = document.createElement('h2')
@@ -27,22 +28,25 @@ function fetchWeatherData () { //retrieves user input and attaches it to url as 
     var weatherCurrentTemp = document.createElement('p')
     var weatherWind = document.createElement('p')
     var weatherHumidity = document.createElement('p')
+    var weatherUV = document.createElement('p')
       
-    // weatherDate.textContent = moment().format("dddd, MMMM Do")
+    weatherDate.textContent = moment().format("dddd, MMMM Do")
     weatherDate.classList.add("current-weather-date")
-  
-    locationInputTitle.textContent = locationInput
+
+    locationInputTitle.textContent = locationInput.value
 
     var iconURL = "https://openweathermap.org/img/wn/"
-    // iconimport = iconURL.concat(icon)
-    // weatherIcon.setAttribute("src" , iconimport+".png")
+    iconimport = iconURL.concat(icon)
+    weatherIcon.setAttribute("src" , iconimport+".png")
     weatherIcon.classList.add("weather-icon");
 
     weatherCurrentTemp.textContent = "Current Temp: " + forecast.temp
-    weatherWind.textContent = "Wind: " + forecast.wind
+    weatherWind.textContent = "Wind: " + wind.speed + " mph"
     weatherHumidity.textContent = "Humidity: " + forecast.humidity
+    weatherUV.textContent = "UV Index: " + forecast.uvindex
+    weatherUV.classList.add("uv-index")
 
-    dayContainer.append(locationInputTitle, weatherDate, weatherIcon, weatherCurrentTemp, weatherWind, weatherHumidity)
+    dayContainer.append(locationInputTitle, weatherDate, weatherIcon, weatherCurrentTemp, weatherWind, weatherHumidity, weatherUV)
     dayContainer.append(weatherIcon)
   
   })
